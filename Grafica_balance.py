2d62f0136e2ceaae53cbd8479ac8b8b5fc3a6056
Jose21172 <perezmompean57@gmail.com>
2024-05-17 10:53:14 +0200

    Actualización a Grafica_balance.py y mas comentarios

--- Grafica_balance.py
+++ Grafica_balance.py
@@ -2,23 +2,27 @@ import matplotlib.pyplot as plt
 import sqlite3
 import time
 
+
 # Este fichero se encarga de temas relacionados con el historial monetario
 
-# Creamos la clase que va a almacenar funciones
-class Grafica_balance():
+# Creamos la clase que va a almacenar funciones y poder llamarlas desde otros ficheros
+class Grafica_balance:
 
     # graficar_balance se ocupa de graficar un matplotlib a partir de un DNI
     @staticmethod
     def graficar_balance(dni):
+        # Coge la base de datos
         conn = sqlite3.connect('hist_bal.db')
         cursor = conn.cursor()
+        # Pilla los valores que queremos
         cursor.execute('SELECT dinero FROM hist_bal WHERE dni = ?', (str(dni),))
+        # Obtiene la columna
         user = cursor.fetchall()
         a = ([i[0] for i in user])
         print(a)
         conn.close()
         lista_aux = []
-
+        # Crea una lista con su longitud
         for i in range(0, len(a)):
             lista_aux.append(i)
 
@@ -31,7 +35,6 @@ class Grafica_balance():
 
         plt.savefig('Imagenes/hist_bal.png')
         plt.show()
-
 
     @staticmethod
     def meter_datos_bd(DNI, dinero):
@@ -62,6 +65,7 @@ class Grafica_balance():
             print("Error al dar de baja:", e)
         finally:
             conn.close()
+
 
 if __name__ == '__main__':
     Grafica_balance.graficar_balance(257)
