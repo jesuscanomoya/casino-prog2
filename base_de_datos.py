b30e709a9625208073ee212fd40dc77acadc4367
Jose21172 <106876543+Jose21172@users.noreply.github.com>
2024-05-21 20:52:30 +0200

    Update base_de_datos.py

--- base_de_datos.py
+++ base_de_datos.py
@@ -4,18 +4,9 @@ Creación Base de Datos
 Este archivo se encarga de crear una base de datos para la gestión de usuarios del casino.
 Se creará una tabla ('usuarios') en la que guardar los datos de todos los usuarios. Estos
 datos podrán ser modificados por el programa de gestión de usuarios 'usuario.py'.
-
-Atributos
----------
-dni: str
-    DNI/NIE del usuario. Identificador del usuario en el programa.
-nombre: str
-    Nombre del usuario.
-apellidos: str
-    Apellido/s del usuario.
-contraseña: str
-    Contraseña del usuario. Necesaria para iniciar sesión.
-    Codificada mediante la función sha256 de la librería hashlib.
+Por otro lado, se creará una tabla 'hist_bal' que guardará las modificaciones de capital de
+todos los usuarios. De esta tabla obtendremos el historial de cada usuario en el archivo
+'Grafica_balance.py'.
 """
 
 import sqlite3
@@ -39,6 +30,17 @@ def bd_crear():
         Dinero del que dispone el usuario.
     prohibido: boolean, por defecto False
         Indica si el usuario tiene la entrada prohibida.
+
+    Atributos de la tabla 'hist_bal'
+    --------------------------------
+    id: integer, valor en autoincremento, clave primaria
+        Id de la modificación. Añade valor automático.
+    tiempo: float, valor no nulo
+        Momento en el que se realiza la modificación. Recibido de time.time().
+    dni: varchar, longitud 9, valor no nulo
+        DNI/NIE del usuario. Clave ajena procedente de 'usuarios'.
+    dinero: integer, por defecto 10
+        Dinero del que dispone el usuario (modificación). Clave ajena procedente de 'usuarios'.
     """
     conn = sqlite3.connect('usuarios.db')
     cursor = conn.cursor()
@@ -53,7 +55,18 @@ def bd_crear():
         );
     ''')
     conn.commit()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS hist_bal (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            tiempo FLOAT NOT NULL,
+            dni VARCHAR(9) NOT NULL,
+            dinero INTEGER DEFAULT 10,
+            FOREIGN KEY (dni, dinero) REFERENCES usuarios(dni, dinero)
+        );
+    ''')
+    conn.commit()
     conn.close()
 
 
 bd_crear()
+
