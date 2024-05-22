6218882338d3c561cc9bac174da4da8264c49a40
jesuscanomoya <168172293+jesuscanomoya@users.noreply.github.com>
2024-05-22 13:19:33 +0200

    Update Grafica_balance.py

--- Grafica_balance.py
+++ Grafica_balance.py
@@ -5,7 +5,6 @@ import time
 
 # Este fichero se encarga de temas relacionados con el historial monetario
 
-# Creamos la clase que va a almacenar funciones y poder llamarlas desde otros ficheros
 class Grafica_balance:
 
     # graficar_balance se ocupa de graficar un matplotlib a partir de un DNI
@@ -37,38 +36,6 @@ class Grafica_balance:
         # Guarda la imagen para que se pueda utilizar luego
         plt.savefig('Imagenes/hist_bal.png')
         plt.show()
-
-    # Esta función se encarga de meter datos en la bd para que se puedan usar
-    @staticmethod
-    def meter_datos_bd(DNI, dinero):
-        conn = sqlite3.connect('hist_bal.db')
-        cursor = conn.cursor()
-        # Mute los valores y mira que no pasé nada
-        try:
-            cursor.execute('INSERT INTO hist_bal (dni, dinero, tiempo) VALUES (?, ?, ?)',
-                           (DNI, dinero, time.time()))
-            conn.commit()
-            print("Todo sucedió bien")
-        except sqlite3.IntegrityError:
-            print("Esto no debería pasar")
-        except sqlite3.OperationalError as e:
-            print("Error operacional:", e)
-        finally:
-            conn.close()
-
-    # Como dice su título elimina el user en la bd usando DELETE
-    @staticmethod
-    def eliminar_usuario(DNI):
-        conn = sqlite3.connect('hist_bal.db')
-        cursor = conn.cursor()
-        try:
-            cursor.execute('DELETE FROM hist_bal WHERE dni = ?', (DNI,))
-            conn.commit()
-            print("Usuario dado de baja correctamente.")
-        except Exception as e:
-            print("Error al dar de baja:", e)
-        finally:
-            conn.close()
 
 
 if __name__ == '__main__':
